6ddf5a5479874e6e7972f66c87ef192a93611625
irvanfebri <padcyber@gmail.com>
2022-10-19 16:35:33 +0700

    backend

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    //mass assigment
+    protected $fillable = [
+        'name',
+        'logo'
+    ];
 }
