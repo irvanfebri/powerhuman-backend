ed95395bc8ce49dca9f3974debf106071668aabb
irvanfebri <padcyber@gmail.com>
2022-10-28 10:01:00 +0700

    power

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,11 @@ use App\Http\Controllers\API\UserController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 
 Route::get('/company',[CompanyController::class,'all']);
 
 Route::post('login',[UserController::class, 'login']);
+Route::post('register',[UserController::class, 'register']);
+Route::post('logout',[UserController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('user',[UserController::class, 'fetch'])->middleware('auth:sanctum');
